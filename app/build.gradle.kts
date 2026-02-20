2fa8722902d6ff9d4d5d2f0c0ee906fe05c3f5d6
dhlee-brain <dhlee9208@gmail.com>
2026-02-20 11:14:28 +0900

    260220

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,8 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.datetime.wheel.picker)
-    implementation(libs.kotlinx.datetime)
+    implementation(libs.picktime.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
